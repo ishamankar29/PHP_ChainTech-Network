2d7ef60afe0a18e119556d3066c7d9dacae1c041
Isha Mankar <118759023+ishamankar29@users.noreply.github.com>
2024-02-03 23:01:28 +0530

    Web App Routes

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [RegisterController::class, 'create']);
+Route::post('/register', [RegisterController::class, 'store'])->name('register');
+Route::get('/login', [LoginController::class, 'index'])->name('login.index');
+Route::post('/check', [LoginController::class, 'check'])->name('check');
+Route::get('/direct-login', [RegisterController::class, 'directLogin'])->name('directLogin');
